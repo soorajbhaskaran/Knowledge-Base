233f02baaea9128a837b9078a00ae031c06a7614
Sooraj Bhaskaran <sooraj.bhaskaran@bigbinary.com>
2022-10-11 02:56:46 +0530

    Added git hooks, eslint, prettier and rubocop (#4)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-    def index
-        render
-    end
+  def index
+    render
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,4 @@ var ReactRailsUJS = require("react_ujs");
 
 ReactRailsUJS.useContext(componentRequireContext);
 
+
