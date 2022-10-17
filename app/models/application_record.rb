5b35ef1a670ce574b43a5e5dffde4c2eac83c6c7
Sooraj Bhaskaran <sooraj.bhaskaran@bigbinary.com>
2022-10-17 23:04:52 +0530

    Fixed error in error handler (#42)

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
+  include ActionView::Helpers::TranslationHelper
   self.abstract_class = true
+
+  def errors_to_sentence
+    errors.full_messages.to_sentence
+  end
 end
