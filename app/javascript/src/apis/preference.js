7dbb58661026cd2f76686731945da2aef1a1ce75
Sooraj Bhaskaran <sooraj.bhaskaran@bigbinary.com>
2022-11-10 15:07:40 +0530

    Added organization to frontend

--- app/javascript/src/apis/preference.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const show = () => axios.get("/preference");
-const update = (payload) => axios.put("/preference/", payload);
-
-const preferenceApi = { show, update };
-
-export default preferenceApi;
