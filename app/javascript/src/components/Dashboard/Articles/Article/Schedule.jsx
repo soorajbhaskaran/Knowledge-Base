b5df1c82b9c5a78300c57a607ba4087299c1e52a
Sooraj Bhaskaran <sooraj.bhaskaran@bigbinary.com>
2022-12-15 11:31:23 +0530

    Improved UX of schedule pane (#270)

--- app/javascript/src/components/Dashboard/Articles/Article/Schedule.jsx
+++ app/javascript/src/components/Dashboard/Articles/Article/Schedule.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 
+import dayjs from "dayjs";
 import { Pane, Typography, DatePicker, TimePicker, Button } from "neetoui";
-import { assoc } from "ramda";
+import { assoc, range } from "ramda";
 
 const Schedule = ({ showPane, setShowPane, status, handleCreateSchedule }) => {
   const [dateTime, setDateTime] = useState({ date: "", time: "" });
+  const scheduleStatus = status === "published" ? "Publish" : "Unpublish";
 
   const handleSubmit = () => {
     handleCreateSchedule(dateTime);
@@ -16,23 +18,27 @@ const Schedule = ({ showPane, setShowPane, status, handleCreateSchedule }) => {
     setDateTime({ date: "", time: "" });
   };
 
+  const getDisabledHours = () => range(0, dayjs().hour());
+
+  const getDisabledDate = current =>
+    current.isBefore(dayjs().subtract(1, "day"));
+
   return (
     <Pane isOpen={showPane} onClose={onClose}>
       <Pane.Header>
-        <Typography style="h3">Schedule article</Typography>
+        <Typography style="h3">{scheduleStatus} article</Typography>
       </Pane.Header>
       <Pane.Body className="flex flex-col space-y-6">
         <div className="w-full">
           <DatePicker
-            required
             className="mb-4"
-            getPopupContainer={triggerNode => triggerNode.parentNode}
+            disabledDate={getDisabledDate}
             label="Select Date"
             onSelect={value => setDateTime(assoc("date", value))}
           />
           <TimePicker
-            required
             use12Hours
+            disabledHours={getDisabledHours}
             format="HH:mm:ss"
             label="Select Time"
             type="time"
@@ -43,7 +49,7 @@ const Schedule = ({ showPane, setShowPane, status, handleCreateSchedule }) => {
       <Pane.Footer>
         <Button
           className="mr-3"
-          label={status === "draft" ? "Publish later" : "Unpublish later"}
+          label="Set schedule"
           size="large"
           type="submit"
           onClick={handleSubmit}
