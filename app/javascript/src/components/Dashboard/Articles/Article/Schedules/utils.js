f541391fcae3be8db06ea103d5cbf8b0bdc7a423
Sooraj Bhaskaran <sooraj.bhaskaran@bigbinary.com>
2022-12-19 12:53:07 +0530

    Enhanced UI of scribble

--- app/javascript/src/components/Dashboard/Articles/Article/Schedules/utils.js
+++ app/javascript/src/components/Dashboard/Articles/Article/Schedules/utils.js
@@ -1,7 +1,4 @@
 import dayjs from "dayjs";
-import { range } from "ramda";
-
-export const getDisabledHours = () => range(0, dayjs().hour());
 
 export const getDisabledDate = current =>
   current.isBefore(dayjs().subtract(1, "day"));
