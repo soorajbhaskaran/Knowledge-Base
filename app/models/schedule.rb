1f6bc7f7feb5e790c13035484d5f1e65b35a2825
Sooraj Bhaskaran <sooraj.bhaskaran@bigbinary.com>
2022-12-22 22:02:16 +0530

    Converted API functions to active record callbacks (#309)

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "sidekiq/api"
+
 class Schedule < ApplicationRecord
   belongs_to :article
 
@@ -7,14 +9,16 @@ class Schedule < ApplicationRecord
   validate :schedule_cannot_be_in_the_past, on: :create
   validate :status_cannot_be_the_same, on: :create
 
-  def remove_job_from_queue
-    scheduled_queue = Sidekiq::ScheduledSet.new
-    scheduled_queue.scan("ArticleUpdateWorker").each do |job|
-      job.delete if job.args.last == self.id
-    end
-  end
+  before_destroy :remove_job_from_queue, prepend: true
 
   private
+
+    def remove_job_from_queue
+      scheduled_queue = Sidekiq::ScheduledSet.new
+      scheduled_queue.scan("ArticleUpdateWorker").each do |job|
+        job.delete if job.args.last == self.id
+      end
+    end
 
     def schedule_cannot_be_in_the_past
       return unless scheduled_at <= Time.zone.now
